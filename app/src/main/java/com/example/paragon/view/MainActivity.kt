232b45aa01c1386bd7e8e42685dd522a8e010118
burakkalindamar <burakkalindamar10@gmail.com>
2024-05-04 14:25:34 +0300

    güncellemeHHHH

--- app/src/main/java/com/example/paragon/view/MainActivity.kt
+++ app/src/main/java/com/example/paragon/view/MainActivity.kt
@@ -1,23 +1,28 @@
-package com.example.paragon
+package com.example.paragon.view
 
+import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.paragon.R
 import com.example.paragon.databinding.ActivityMainBinding
-import com.example.paragon.databinding.ActivitySearchPageBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var db : SQLiteDatabase
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        binding() //binding işlemlemleri
+        database() // db ve tablo oluştur
+        //db.execSQL("INSERT INTO table1 VALUES ('APPLE' , 'APLL', 170.38 , 10.38)")
+        //db.execSQL("INSERT INTO table1 VALUES ('BANK OF AMERİCA' , 'BAC', 32.62 , 38.57)")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -26,14 +31,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun go_search(view: View){
-        val go_search = Intent(this,Search_page::class.java)
+        val go_search = Intent(this, Search_page::class.java)
         startActivity(go_search)
     }
 
     fun refresh_balance(view: View){
         finish()
-        val refresh_balance = Intent(this,MainActivity::class.java)
+        val refresh_balance = Intent(this, MainActivity::class.java)
         startActivity(refresh_balance)
     }
 
-}ö
+    fun binding(){
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun database(){
+        context=this
+        db=context.openOrCreateDatabase("testdb",Context.MODE_PRIVATE,null)
+        db.execSQL("CREATE TABLE IF NOT EXISTS table1(stocks TEXT, symbol TEXT, buying_price REAL, shares REAL)")
+
+    }
+
+}
--- app/src/main/java/com/example/paragon/view/Search_page.kt
+++ app/src/main/java/com/example/paragon/view/Search_page.kt
@@ -1,4 +1,4 @@
-package com.example.paragon
+package com.example.paragon.view
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.paragon.databinding.ActivityMainBinding
+import com.example.paragon.R
 import com.example.paragon.databinding.ActivitySearchPageBinding
 
 class Search_page : AppCompatActivity() {
@@ -15,9 +15,7 @@ class Search_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivitySearchPageBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        binding()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -26,15 +24,22 @@ class Search_page : AppCompatActivity() {
     }
 
     fun go_balance(view: View){
-        val go_balance = Intent(this,MainActivity::class.java)
+        val go_balance = Intent(this, MainActivity::class.java)
         startActivity(go_balance)
     }
 
     fun refresh_search(view: View){
         finish()
-        val refresh_search = Intent(this,Search_page::class.java)
+        val refresh_search = Intent(this, Search_page::class.java)
         startActivity(refresh_search)
     }
+
+    fun binding(){
+        binding=ActivitySearchPageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
 
 
 }
